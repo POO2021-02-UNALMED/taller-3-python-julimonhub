bd6197ac7db884e98d52041d1dbe6190a995aea2
julimonhub <92111604+julimonhub@users.noreply.github.com>
2021-11-02 23:03:28 -0500

    main2

--- televisores/marca.py
+++ televisores/marca.py
@@ -1,2 +1,10 @@
 class Marca:
-    push
+    def__init__(self,nombre):
+       self._nombre=nombre
+
+    def setNombre(self,nombre):
+        self._nombre=nombre
+
+    def getNombre(self):
+
+        return(self._nombre)
